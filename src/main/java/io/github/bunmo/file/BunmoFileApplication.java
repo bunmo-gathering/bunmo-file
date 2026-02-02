e64e94e37a1efd467dc66454039e0b7ee4bc2343
youable <youable.framios@gmail.com>
2026-02-02 13:41:42 +0900

    init project

---
+++ src/main/java/io/github/bunmo/file/BunmoFileApplication.java
@@ -0,0 +1,13 @@
+package io.github.bunmo.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunmoFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BunmoFileApplication.class, args);
+    }
+
+}
